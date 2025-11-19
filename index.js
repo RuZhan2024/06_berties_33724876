9e6fdeb4189d74cf5a5b1d7c4be96ea52125014d
RuZhan2024 <bzruzhan@163.com>
2025-11-19 19:41:03 +0000

    Edit database creating params

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Define the database connection pool
 // Define the database connection pool
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.BB_HOST || "localhost",
+  user: process.env.BB_USER,
+  password: process.env.BB_PASSWORD,
+  database: process.env.BB_DATABASE,
+  port: process.env.BB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
